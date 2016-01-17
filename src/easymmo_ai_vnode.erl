63db902aac6209bd85e48d42f02f934288cd0454
Masatoshi Itoh <masatoshi9953@gmail.com>
2016-01-18 08:51:53 +0900

    working commit (add handoff codes and not tested).

--- src/easymmo_ai_vnode.erl
+++ src/easymmo_ai_vnode.erl
@@ -1,6 +1,7 @@
 -module(easymmo_ai_vnode).
 -behaviour(riak_core_vnode).
 -include("easymmo_ai.hrl").
+-include("riak_core_vnode.hrl").
 
 -export([start_vnode/1,
          init/1,
@@ -96,9 +97,40 @@ handle_command(Message, _Sender, State) ->
     ?PRINT({unhandled_command, Message}),
     {noreply, State}.
 
+%% get list of binnames
+object_list(State) ->
+	dict:fetch_keys(State#state.pids).
+
+convtokey(BinName) ->
+	{<<"easymmo_ai">>, BinName}.
+
+gotvalue(BinName, State) ->
+	local_get_state(BinName, State).
+
+%% see http://jp.basho.com/posts/technical/understanding-riak_core-building-handoff/
+%% object_list function returns a list of the keys to fold over.
+%% write object_list by yourself.
+handle_handoff_command(?FOLD_REQ{foldfun=VisitFun, acc0=Acc0}, _Sender, State) ->
+	ObjectBinNames = object_list(State),
+	%% eliding details for now. Don't worry, we'll get to them shortly.
+	Do = fun(BinName, AccIn) ->
+		K = convtokey(BinName),
+		?PRINT(K),
+		V = gotvalue(BinName, State),
+		?PRINT(V),
+		%%Data = term_to_binary({K, V}),
+		Data = V, %% don't apply XXX_to_binary here.
+		?PRINT(Data),
+		AccOut = VisitFun(K, Data, AccIn),
+		?PRINT(AccOut),
+		AccOut
+	end,
+	Final = lists:foldl(Do, Acc0, ObjectBinNames),
+	{reply, Final, State};
+
 handle_handoff_command(_Message, _Sender, State) ->
-    ?PRINT({handle_handoff_command, _Message}),
-    {noreply, State}.
+	{noreply, State}.
+
 
 handoff_starting(_TargetNode, State) ->
     ?PRINT({handoff_starting, _TargetNode}),
@@ -112,16 +144,37 @@ handoff_finished(_TargetNode, State) ->
     ?PRINT({handoff_finished, _TargetNode}),
     {ok, State}.
 
-handle_handoff_data(_Data, State) ->
-    ?PRINT({handoff_data, _Data}),
-    {reply, ok, State}.
+handle_handoff_data(Data, State) ->
+	{{<<"easymmo_ai">>, BinName} , WorkerState} = binary_to_term(Data),
+	?PRINT({BinName, WorkerState}),
+	NewState = case dict:find(BinName, State#state.pids) of
+		{ok, _ExistingPid} ->
+			State;
+		error ->
+			?PRINT({BinName, WorkerState}),
+			start_and_set_state({BinName, WorkerState}, State)
+	end,
+	{reply, ok, NewState}.
 
-encode_handoff_item(_ObjectName, _ObjectValue) ->
-    ?PRINT({encode_handoff_item, _ObjectName, _ObjectValue}),
-    <<>>.
+%% returns State
+start_and_set_state({BinName, WorkerState}, State) ->
+	?PRINT({start_and_set_state, BinName, WorkerState}),
+	{ok, Pid} = worker:start_link(),
+	{ok, _NewWorkerState} = worker:set_state(Pid, WorkerState),
+	NewPids = dict:store(BinName, Pid, State#state.pids),
+	NewState = State#state{pids = NewPids},
+	NewState.
+
+local_get_state(BinName, State) ->
+	Pid = dict:fetch(BinName, State#state.pids),
+	{ok, Result} = worker:get_state(Pid),
+	Result.
+
+encode_handoff_item(BinKey, Value) ->
+	term_to_binary({BinKey, Value}).
 
 is_empty(State) ->
-    {true, State}.
+	{dict:size(State#state.pids) == 0, State}.
 
 delete(State) ->
     {ok, State}.
